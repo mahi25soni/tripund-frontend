8d42bbde921e946678eac686a23f2e40e413a4b4
mahi25soni <msdraj02@protonmail.com>
2024-07-12 09:54:09 +0530

    Add offers and get all offers api integrated

--- src/components/OfferList.jsx
+++ src/components/OfferList.jsx
@@ -2,47 +2,57 @@ import React, { useState } from "react";
 import { FiMoreVertical } from "react-icons/fi";
 import { IoAddSharp } from "react-icons/io5";
 import { AddOffer } from "./AddOffer";
+import moment from "moment";
 
-export const OfferList = () => {
-    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+export const OfferList = ({ allOffersList }) => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
-    const toggleDropdown = () => {
-      setIsDropdownOpen(!isDropdownOpen);
-    };
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
   return (
-    <div className="flex flex-wrap justify-start">
-      <div className="h-64 w-64 px-5  flex flex-col justify-evenly bg-white">
-        <div className="flex justify-between items-center relative">
-          <div className="text-green-500 bg-green-200 font-bold text-xl p-2 rounded-md">
-            25%
-          </div>
-          <FiMoreVertical className="h-5 w-5"  onClick={toggleDropdown} />
-          {isDropdownOpen &&  <div className="absolute right-6 top-2 bg-white shadow-slate-400 shadow-lg flex flex-col items-start gap-1 p-2 rounded-md">
-            <button className="border-none bg-transparent hover:bg-gray-200 p-1 rounded-md ">
-              Mark as disable
-            </button>
-            <button className="border-none bg-transparent hover:bg-gray-200 p-1 rounded-md">Delete</button>
-            <button className="border-none bg-transparent hover:bg-gray-200 p-1 rounded-md ">Edit</button>
-          </div>}
+    <div className="flex flex-wrap justify-start gap-5">
+      {allOffersList?.map((offer) => {
+        return (
+          <div className="h-64 w-64 px-5  flex flex-col justify-evenly bg-white">
+            <div className="flex justify-between items-center relative">
+              <div className="text-green-500 bg-green-200 font-bold text-xl p-2 rounded-md">
+                {offer?.offer_discount}%
+              </div>
+              <FiMoreVertical className="h-5 w-5" onClick={toggleDropdown} />
+              {isDropdownOpen && (
+                <div className="absolute right-6 top-2 bg-white shadow-slate-400 shadow-lg flex flex-col items-start gap-1 p-2 rounded-md">
+                  <button className="border-none bg-transparent hover:bg-gray-200 p-1 rounded-md ">
+                    Mark as disable
+                  </button>
+                  <button className="border-none bg-transparent hover:bg-gray-200 p-1 rounded-md">
+                    Delete
+                  </button>
+                  <button className="border-none bg-transparent hover:bg-gray-200 p-1 rounded-md ">
+                    Edit
+                  </button>
+                </div>
+              )}
+            </div>
 
-        </div>
-
-        <p className="text-2xl font-normal">Summer Festive Offer</p>
-        <p className="text-[18px] font-normal text-gray-600">
-          Validity : 25 July
-        </p>
-        <div className="flex justify-between items-center">
-          <div>
-            <span className="font-normal mr-1 text-xl">Product </span>
-            <div className="text-orange-500 bg-orange-200 px-2.5 py-1 inline rounded-md font-semibold">
-              15
+            <p className="text-2xl font-normal">{offer?.offer_heading}</p>
+            <p className="text-[18px] font-normal text-gray-600">
+              Validity : { moment(offer?.end_date).format('DD MMMM YYYY')}
+            </p>
+            <div className="flex justify-between items-center">
+              <div>
+                <span className="font-normal mr-1 text-xl">Product </span>
+                <div className="text-orange-500 bg-orange-200 px-2.5 py-1 inline rounded-md font-semibold">
+                  15
+                </div>
+              </div>
+              <button className="p-1 bg-blue-500 rounded-md">
+                <IoAddSharp className="h-6 w-6 font-bold text-white" />
+              </button>
             </div>
           </div>
-          <button className="p-1 bg-blue-500 rounded-md">
-            <IoAddSharp className="h-6 w-6 font-bold text-white" />
-          </button>
-        </div>
-      </div>
+        );
+      })}
     </div>
   );
 };
--- src/pages/Offer/Offer.jsx
+++ src/pages/Offer/Offer.jsx
@@ -1,10 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { OfferList } from "../../components/OfferList";
 import { AddOffer } from "../../components/AddOffer";
 import CardWrapper from "../../atoms/CardWrapper";
+import axios from "../../../axios.jsx"
 
 export const Offer = () => {
   const [addOfferPopUp, setAddOfferPopUp] = useState(false);
+  const [allOffersList, setAllOffersList] = useState([])
+
+  const UserToken = localStorage.getItem("token")
+
+
+  useEffect(() => {
+    (async() => {
+
+      const {data} = await axios.get("/offer/get-all", {
+        headers : {
+            "Authorization" : "Bearer " + UserToken
+        }
+    })
+
+    setAllOffersList(data?.data)
+    })()
+  }, [])
+
+  console.log("offers are ", allOffersList)
+
 
   const openAddOfferPopUp = () => {
     setAddOfferPopUp(true);
@@ -15,16 +36,19 @@ export const Offer = () => {
       <div className="flex flex-col gap-4">
         <CardWrapper
           header_name={"Offers"}
-          value={"5"}
+          value={allOffersList?.length}
           button_name={"Create Offers"}
           button_function={openAddOfferPopUp}
           className="h-[120px]"
           firstChildClasses="h-full"
         ></CardWrapper>
-        <OfferList></OfferList>
+        <OfferList allOffersList={allOffersList}></OfferList>
       </div>
       {}
-      {addOfferPopUp && <AddOffer></AddOffer>}
+      {addOfferPopUp && <AddOffer
+      setAddOfferPopUp = {setAddOfferPopUp}
+      setAllOffersList = {setAllOffersList}
+      ></AddOffer>}
     </>
   );
 };
